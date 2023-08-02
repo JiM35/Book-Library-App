d5ce2faa1d635acf7aa7acd547560d8877e5d460
jamesgitonga <gitongajames35@gmail.com>
2023-08-02 11:14:26 +0300

    Updates table data without necessarily having to refresh the application

--- app/src/main/java/com/example/booklibraryapp/MainActivity.java
+++ app/src/main/java/com/example/booklibraryapp/MainActivity.java
@@ -47,9 +47,17 @@ public class MainActivity extends AppCompatActivity {
 
         storeDataInArrays();
 
-        customAdapter = new CustomAdapter(MainActivity.this, book_id, book_title, book_author, book_pages);
+        customAdapter = new CustomAdapter(MainActivity.this, this, book_id, book_title, book_author, book_pages);
         recyclerView.setAdapter(customAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            recreate();
+        }
     }
 
     void storeDataInArrays(){
